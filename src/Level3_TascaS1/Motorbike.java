9d3c08453a9ea7ce7cea7177fbcea9c5e84678d6
Youssef <youssef.eg09@gmail.com>
2025-09-10 10:36:56 +0200

    add : motorbike subclass created

--- src/Level3_TascaS1/Motorbike.java
+++ src/Level3_TascaS1/Motorbike.java
@@ -3,6 +3,18 @@ package Level3_TascaS1;
 public class Motorbike extends SportsNews{
     private String moto_team;
 
+    public Motorbike(String name, String text, double mark, double price, String moto_team) {
+        super(name, text, mark, price);
+        this.moto_team = moto_team;
+    }
+
+    public String getMoto_team() {
+        return moto_team;
+    }
+
+    public void setMoto_team(String moto_team) {
+        this.moto_team = moto_team;
+    }
 
     @Override
     public double calculateNewsPrice() {
