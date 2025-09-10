6a6f5289bd992d7e2ea3fe8edb05c547bc1a90e4
Youssef <youssef.eg09@gmail.com>
2025-09-10 09:47:39 +0200

    feat: implementing fail-fast in classes

--- src/Ejercicio1_TascaS1/Main.java
+++ src/Ejercicio1_TascaS1/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Wind Flute = new Wind("Jesus Flute", 5000);
         Strings Guitar = new Strings("Paco de Lucia's guitar", 800000);
-        Percussion Bongo = new Percussion("", 40);
+        Percussion Bongo = new Percussion("Bongo", 40);
 
 
         System.out.println(Flute.play() + Flute);
